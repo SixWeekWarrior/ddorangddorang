a7eda4b80d53075a34af0c181417123893bc7c2d
이효식 <lhs9394@naver.com>
2023-10-13 11:15:51 +0900

    Backend Initial Setting Commit

---
+++ backend/src/test/java/com/sww/ddorangddorang/DdorangddorangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sww.ddorangddorang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdorangddorangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
