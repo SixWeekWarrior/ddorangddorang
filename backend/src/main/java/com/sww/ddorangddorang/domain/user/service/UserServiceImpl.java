689b679956997798c201aa576458266f60bd1cce
이효식 <lhs9394@naver.com>
2023-10-20 13:40:32 +0900

    Feat: User entity 추가
    User entity 작성 완료
    Api, service 구조 생성

---
+++ backend/src/main/java/com/sww/ddorangddorang/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sww.ddorangddorang.domain.user.service;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Transactional
+@Service
+public class UserServiceImpl implements UserService {
+
+}
