3036d7757a1194c61074f9b2c28ddcb266ef8949
baiksungil <sibaek31@gmail.com>
2023-10-26 17:15:38 +0900

    Feat: 임시 생성자

--- backend/src/main/java/com/sww/ddorangddorang/domain/user/entity/User.java
+++ backend/src/main/java/com/sww/ddorangddorang/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -33,4 +34,11 @@ public class User {
     private Long participate;           // BIGINT - FK(ROOM)
     private Long status;                // BIGINT - FK(MASTER_CODE)
     private LocalDateTime deletedAt;    // TIMESTAMP
+
+    @Builder
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 }
