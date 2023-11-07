93687c0d110c5839d6d701d36a327794cedd1986
HongJaeyeon <ddrrff@kookmin.ac.kr>
2023-11-07 10:10:45 +0900

    FIX: 이름 변경 및 설명 추

--- frontend/src/modules/user.ts
+++ frontend/src/modules/user.ts
@@ -3,7 +3,9 @@ import {UserInfo} from '../types/user';
 
 const KEY = 'USER';
 
-export const JoinUserInfoState = atom<UserInfo>({
+// 수정 중인 정보를 가진다.
+// 이 값은 실제 DB와 다를 수 있다.
+export const TmpUserInfoState = atom<UserInfo>({
   key: `${KEY}/info`,
   default: {
     name: '',
@@ -23,6 +25,7 @@ export const JoinUserInfoState = atom<UserInfo>({
   },
 });
 
+// 힝상 DB와 같은 정보를 가진다.
 export const UserInfoState = atom<UserInfo>({
   key: `${KEY}/info`,
   default: {
@@ -43,6 +46,6 @@ export const UserInfoState = atom<UserInfo>({
   },
 });
 
-const user = {UserInfoState, JoinUserInfoState};
+const user = {UserInfoState, TmpUserInfoState};
 
 export default user;
