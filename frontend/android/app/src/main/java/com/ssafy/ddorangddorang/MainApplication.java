c11b9385ed620946a30e1f065cbb999ae18242ee
이연주 <legrandreve@naver.com>
2023-11-08 13:17:19 +0900

    Feat: initial route 변경

--- frontend/android/app/src/main/java/com/ssafy/ddorangddorang/MainApplication.java
+++ frontend/android/app/src/main/java/com/ssafy/ddorangddorang/MainApplication.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =r
+  private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
