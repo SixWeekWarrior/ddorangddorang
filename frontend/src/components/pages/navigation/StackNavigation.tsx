aa2041fcbd456488c9f9fe6c2dc6b3f36d5cddd4
이연주 <legrandreve@naver.com>
2023-11-08 14:33:55 +0900

    Feat: missionComplete 완료 UI 추가

--- frontend/src/components/pages/navigation/StackNavigation.tsx
+++ frontend/src/components/pages/navigation/StackNavigation.tsx
@@ -24,7 +24,7 @@ const StackNavigation = () => {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MissionToday">
+      <Stack.Navigator initialRouteName="NavBar">
         <Stack.Screen
           name="Onboarding"
           component={Onboarding}
