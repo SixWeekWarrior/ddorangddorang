5346b47ac6f2dc43e8ff181e12a659a86779541e
박우현 <0x847769a@gmail.com>
2023-11-14 13:59:27 +0900

    Feat: Guess 서비스 구현
    3일 전부터 사용자를 추측하는 API, 내 마니또 추측 현활 및 전체 마니또 추측 현황 구현 완료

--- backend/src/main/java/com/sww/ddorangddorang/domain/guess/api/GuessApi.java
+++ backend/src/main/java/com/sww/ddorangddorang/domain/guess/api/GuessApi.java
@@ -1,18 +1,16 @@
 package com.sww.ddorangddorang.domain.guess.api;
 
 import com.sww.ddorangddorang.auth.dto.AuthenticatedUser;
-import com.sww.ddorangddorang.domain.guess.dto.GuessUserPostRes;
-import com.sww.ddorangddorang.domain.guess.entity.Guess;
+import com.sww.ddorangddorang.domain.guess.dto.GuessStatus;
+import com.sww.ddorangddorang.domain.guess.dto.Profile;
 import com.sww.ddorangddorang.domain.guess.service.GuessService;
 import com.sww.ddorangddorang.global.common.CommonResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,12 +24,29 @@ public class GuessApi {
     private final static String SUCCESS = "SUCCESS";
 
     @PostMapping
-    public CommonResponse<GuessUserPostRes> guessManito(
+    public CommonResponse<Profile> guessManito(
         @AuthenticationPrincipal AuthenticatedUser authenticatedUser, Long manitoId) {
         log.info("GuessApi_guessManito start");
-        GuessUserPostRes guessUserPostRes = guessService.guessManito(authenticatedUser.getId(),
-            manitoId);
+        Profile profile = guessService.guessManito(authenticatedUser.getId(), manitoId);
         log.info("GuessApi_guessManito end");
-        return CommonResponse.success(guessUserPostRes);
+        return CommonResponse.success(profile);
+    }
+
+    @GetMapping
+    public CommonResponse<GuessStatus> myGuessStatus(
+        @AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        log.info("GuessApi_myGuessStatus start");
+        GuessStatus guessStatus = guessService.getMyGuessStatus(authenticatedUser.getId());
+        log.info("GuessApi_myGuessStatus end");
+        return CommonResponse.success(guessStatus);
+    }
+
+    @GetMapping("/all")
+    public CommonResponse<List<GuessStatus>> allGuessStatus(
+        @AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        log.info("GuessApi_allGuessStatus start");
+        List<GuessStatus> guessStatusList = guessService.getAllGuessStatus(authenticatedUser.getId());
+        log.info("GuessApi_allGuessStatus end");
+        return CommonResponse.success(guessStatusList);
     }
 }
