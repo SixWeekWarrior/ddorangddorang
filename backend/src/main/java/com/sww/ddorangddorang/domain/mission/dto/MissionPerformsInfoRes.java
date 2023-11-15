da0f4ed49fdf622ba6a3bcb8847a97ce0a6cc328
박우현 <0x847769a@gmail.com>
2023-11-15 14:04:26 +0900

    Fix: GetMissionByUser list에 dayCount 정보 추가

--- backend/src/main/java/com/sww/ddorangddorang/domain/mission/dto/MissionPerformsInfoRes.java
+++ backend/src/main/java/com/sww/ddorangddorang/domain/mission/dto/MissionPerformsInfoRes.java
@@ -3,6 +3,7 @@ package com.sww.ddorangddorang.domain.mission.dto;
 import com.sww.ddorangddorang.domain.mission.entity.MissionPerform;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -17,23 +18,38 @@ public class MissionPerformsInfoRes {
     private String content;
     private Boolean isComplete;
     private Long missionType;
+    private Integer dayCount;
 
-    public static MissionPerformsInfoRes of(MissionPerform missionPerform) {
-        // Fetch Join이나 Entity Graph 안쓰면 N+1 문제가 발생한다.
-
-        return MissionPerformsInfoRes.builder()
-            .missionId(missionPerform.getId())
-            .title(missionPerform.getMission().getTitle())
-            .content(missionPerform.getMission().getContent())
-            .isComplete(missionPerform.getPerformedAt() != null)
-            .missionType(missionPerform.getMission().getMissionType())
-            .build();
-    }
+//    public static MissionPerformsInfoRes of(MissionPerform missionPerform) {
+//        // Fetch Join이나 Entity Graph 안쓰면 N+1 문제가 발생한다.
+//        return MissionPerformsInfoRes.builder()
+//            .missionId(missionPerform.getId())
+//            .title(missionPerform.getMission().getTitle())
+//            .content(missionPerform.getMission().getContent())
+//            .isComplete(missionPerform.getPerformedAt() != null)
+//            .missionType(missionPerform.getMission().getMissionType())
+//            .build();
+//    }
 
     public static List<MissionPerformsInfoRes> listOf(List<MissionPerform> missionPerforms) {
-        return missionPerforms.stream()
-            .map(MissionPerformsInfoRes::of)
+        return IntStream.range(0, missionPerforms.size())
+            .mapToObj(index -> {
+                MissionPerform missionPerform = missionPerforms.get(index);
+                return MissionPerformsInfoRes.builder()
+                    .missionId(missionPerform.getId())
+                    .title(missionPerform.getMission().getTitle())
+                    .content(missionPerform.getMission().getContent())
+                    .isComplete(missionPerform.getPerformedAt() != null)
+                    .missionType(missionPerform.getMission().getMissionType())
+                    .dayCount(index + 1) // 인덱스는 0부터 시작하므로 1을 더해줌
+                    .build();
+            })
             .collect(Collectors.toList());
     }
+//    public static List<MissionPerformsInfoRes> listOf(List<MissionPerform> missionPerforms) {
+//        return missionPerforms.stream()
+//            .map(MissionPerformsInfoRes::of)
+//            .collect(Collectors.toList());
+//    }
 
 }
